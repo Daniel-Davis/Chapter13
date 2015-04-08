e88641b80911c5483f2c24a4a98c9c1bad6ae5e8
Daniel-Davis <ddv9461@saintjoe.edu>
2015-04-08 12:06:59 -0500

    Update seqSearch.js

--- seqSearch.js
+++ seqSearch.js
@@ -1,1 +1,10 @@
 // Daniel Davis, seqSearch, performs sequential search //
+
+function seqSearch(arr, data) {
+  for (var i=0, i < arr.length, ++i) {
+    if (arr[i]==data) {
+      return true;
+    }// end of if statement
+  }// end of for loop
+  return false;
+}// end of function

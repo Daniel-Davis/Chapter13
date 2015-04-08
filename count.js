de26044c525abafff054a9234b9ccd98e64d75f3
Daniel-Davis <ddv9461@saintjoe.edu>
2015-04-08 12:48:02 -0500

    Update count.js

--- count.js
+++ count.js
@@ -1,2 +1,25 @@
 // Daniel Davis, count, counts amount of accurances//
-
+function count(arr, data) {
+  var count = 0;
+  var position = binSearch(arr, data);
+  if (position > -1) {
+    ++count;
+    for(var i = position-1; i > 0, ++i) {
+      if(arr[i] == data) {
+        ++count;
+      }// end of if statement
+      else {
+        break;
+      }// end of else
+    }// end of for loop
+    for(var i = position+1, i < arr.length, ++i) {
+      if(arr[i] == data) {
+        ++count;
+      }// end of if
+      else {
+        break;
+      }// end of else
+    }// end of for loop
+  }// end of if
+  return count;
+}// end of count

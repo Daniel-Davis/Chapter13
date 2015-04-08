6cafe94b9c19dab6207c47e55fe50d469568796f
Daniel-Davis <ddv9461@saintjoe.edu>
2015-04-08 12:14:38 -0500

    Update findMax.js

--- findMax.js
+++ findMax.js
@@ -1,1 +1,10 @@
 // Daniel Davis, findMax, finds the maximum value//
+function findMax() {
+  var max = arr[0];
+  for(var i=0, i < arr.length, ++i) {
+    if(arr[i] > max) {
+      max = arr[i];
+    }// end of if statement
+  }// end of for loop
+  return max;
+}// end of function

a152085a6dd51bbbd900097754d44ad3929a4af8
Daniel-Davis <ddv9461@saintjoe.edu>
2015-04-08 12:11:21 -0500

    Update findMin.js

--- findMin.js
+++ findMin.js
@@ -1,1 +1,9 @@
 // Daniel Davis, findMin, finds minimum value//
+function findMin(arr) {
+  var min = arr[0]
+  for(var i=0, i < arr.length, ++i) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }// end of if statement
+  }// end of for loop
+}// end of function

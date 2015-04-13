fa49998ed2b74313a9d6ab95e778cfb22f59fd29
Daniel-Davis <ddv9461@saintjoe.edu>
2015-04-13 11:13:48 -0500

    Update findSecondMin.js

--- findSecondMin.js
+++ findSecondMin.js
@@ -1,11 +1,18 @@
 // Daniel Davis, findSecondMin, finds the second smallest element //
 
 function findSecondMin(arr) {
-var min = arr[0]
+  
+var min = arr[0];
+var output = arr[1];
+
 for(var i=0, i < arr.length, ++i) {
-if (arr[i] < min) {
-min = arr[i];
-}// end of if statement
+  if (arr[i] < min) {
+    min = arr[i];
+  }// end of if statement
+  if (arr[i] < output && arr[i] > min) {
+    output = arr[i];
+  }// end of if statement
 }// end of for loop
 return min;
+return output;
 }// end of function
